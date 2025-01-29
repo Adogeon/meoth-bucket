9194c3cf3a3a07ced09305cb4bdbcf6317cc9803
Anh Dao <addao823@gmail.com>
2025-01-29 14:22:51 -0800

    Add base jinga template

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
 @bp.route('/register', method=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -30,13 +31,14 @@ def register():
                 )
                 db.commit()
             except db.IntegrityError:
-                error= f"User {username} is already registered."
+                error = f"User {username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
-        
+
         flash(error)
 
     return render_template('auth/register.html')
+
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
@@ -46,7 +48,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-                'SELECT * FROM user WHERE username = ?', (username,)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
@@ -63,18 +65,31 @@ def login():
 
     return render_template('auth/login.html')
 
+
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id)
+    user_id = session.get('user_id')
 
     if user_id is None:
         g.user = None
     else:
         g.user = get_db().execute(
-                'SELECT * FROM user WHERE id = ?', (user_id,)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
+
 
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
