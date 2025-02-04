e5d8e33e26b5037843a0a925044c96f04255b654
Anh Dao <addao823@gmail.com>
2025-02-03 18:52:27 -0800

    Draft route for add new transaction

--- flaskr/diary.py
+++ flaskr/diary.py
@@ -13,6 +13,35 @@ bp = Blueprint('diary', __name__)
 def index():
     db = get_db()
     transactions = db.execture(
-        'SELECT * FROM transaction t JOIN user u ON t.user_id = u.id ORDER BY created DESC'
+        'SELECT * '
+        'FROM transaction t JOIN user u ON t.user_id = u.id '
+        'ORDER BY created DESC'
     ).fetchall()
     return render_template('diary/index.html', transations=transactions)
+
+
+@bp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        amount = request.form['amount']
+        category = request.form['category']
+        error = None
+
+        if not name:
+            error = 'Transaction name is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO transaction (name, amount, category, user_id)'
+                'VALUES (?, ?, ?)',
+                (name, amount, category, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('diary.index'))
+
+    return render_template('diary/create.html')
